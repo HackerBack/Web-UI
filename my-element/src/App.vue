52c973e77dba40a86f3ae44b38e276be96aa3d87
HackerBack <354077334@qq.com>
2024-08-14 15:16:06 +0800

    feat：二次封装Icon组件、添加Icon组件至Button、Collapse

--- my-element/src/App.vue
+++ my-element/src/App.vue
@@ -3,6 +3,7 @@ import { ref, onMounted } from 'vue'
 import Button from './components/Button/Button.vue'
 import Collapse from './components/Collapse/Collapse.vue'
 import CollapseItem from './components/Collapse/CollapseItem.vue'
+import Icon from './components/Icon/Icon.vue'
 import type { ButtonInstance } from './components/Button/types'
 const buttonRef = ref<ButtonInstance | null>(null)
 const openedValue = ref(['a'])
@@ -17,7 +18,8 @@ onMounted(() => {
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
   </header>
-  <font-awesome-icon icon="fa-solid fa-user-secret" />
+  <!-- <Icon icon="fa-solid fa-user-secret" /> -->
+  <Icon icon="arrow-up" size="2xl" type="danger" color="yellow" />
   <main>
     <Button ref="buttonRef">Test Button</Button>
     <Button plain>Plain Button</Button>
@@ -35,7 +37,9 @@ onMounted(() => {
     <Button type="warning" plain>Warning</Button>
     <Button type="danger" plain>Danger</Button><br /><br />
     <Button size="large">Large</Button>
-    <Button size="small">Small</Button>
+    <Button size="small">Small</Button><br /><br />
+    <Button size="large" loading>Loading</Button>
+    <Button size="large" icon="arrow-up">Icon</Button>
 
     <Collapse v-model="openedValue" accordion>
       <CollapseItem name="a">
--- my-element/src/main.ts
+++ my-element/src/main.ts
@@ -3,10 +3,7 @@ import App from './App.vue'
 import './styles/index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import './styles/index.css'
 
 library.add(fas)
 createApp(App)
-.component('font-awesome-icon', FontAwesomeIcon)
 .mount('#app')
